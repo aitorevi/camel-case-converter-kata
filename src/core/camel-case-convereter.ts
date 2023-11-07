96dd8815def08b0d343d7a971334b7bb18493887
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-11-07 13:33:04 +0100

    :recycle: extract method with replace the first char of the word to upper case

--- src/core/camel-case-convereter.ts
+++ src/core/camel-case-convereter.ts
@@ -1,7 +1,11 @@
+function upperCaseTheFirstCharToTheWord(word: string) {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+}
+
 export function camelCaseConverter(text: string) {
     let splitterRegex = /[ _-]/;
     let words = text.split(splitterRegex);
     let word = words[0];
-    words[0] = word.charAt(0).toUpperCase() + word.substring(1)
+    words[0] = upperCaseTheFirstCharToTheWord(word)
     return words.join("");
 }
