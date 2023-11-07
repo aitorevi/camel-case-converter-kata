f7c77638b73ee995450fe2c1b840fd913d0f3470
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-11-07 14:03:52 +0100

    :recycle: add types in variables and methods, remove splitterRegex variable and sort code

--- src/core/camel-case-convereter.ts
+++ src/core/camel-case-convereter.ts
@@ -1,14 +1,10 @@
-function upperCaseTheFirstCharToTheWords(word: string) {
-    return word.charAt(0).toUpperCase() + word.substring(1);
-}
 
-export function camelCaseConverter(text: string) {
-    let splitterRegex = /[ _-]/;
-    let words = text.split(splitterRegex);
-
-    const wordsToUpperCase = words.map(word =>
-        upperCaseTheFirstCharToTheWords(word)
-    )
+export function camelCaseConverter(text: string): string {
+    const words: string[] = text.split(/[ _-]/);
+    const wordsToUpperCase: string[] = words.map(word => upperCaseTheFirstCharToTheWords(word))
 
     return wordsToUpperCase.join("");
+}
+function upperCaseTheFirstCharToTheWords(word: string): string {
+    return word.charAt(0).toUpperCase() + word.substring(1);
 }
