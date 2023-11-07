9d295764c9e5e4f5ad3f452926bc7bc92c969e98
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-11-07 13:31:17 +0100

    Test(:white_check_mark:): camel case converter convert to a word by replace the first char to upper case

--- src/core/camel-case-convereter.ts
+++ src/core/camel-case-convereter.ts
@@ -1,5 +1,7 @@
 export function camelCaseConverter(text: string) {
     let splitterRegex = /[ _-]/;
     let words = text.split(splitterRegex);
+    let word = words[0];
+    words[0] = word.charAt(0).toUpperCase() + word.substring(1)
     return words.join("");
 }
